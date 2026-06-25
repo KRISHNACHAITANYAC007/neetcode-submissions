ee2fd5f0d777d6f9b6f96d6104bcbe4287ae2fc8
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-06-25 12:32:06 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string k = "";
+
+        for(char c : s)
+        {
+            if(isalnum(c))
+                k += tolower(c);
+        }
+
+        int l = 0;
+        int r = k.length()-1;
+
+        while(l < r)
+        {
+            if(k[l] != k[r])
+                return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
