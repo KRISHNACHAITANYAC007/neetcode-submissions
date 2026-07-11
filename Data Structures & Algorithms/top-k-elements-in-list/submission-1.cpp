3997002c68ee252ce32a45534acef3232f50b427
CHILLAPPAGARI KRISHNA CHAITANYA <ckrishnachaitanya007@gmail.com>
2026-07-11 17:03:54 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       unordered_map<int,int> mp;
+
+               for(int x : nums)
+                       {
+                                   mp[x]++;
+                                           }
+
+                                                   vector<vector<int>> bucket(nums.size() + 1);
+
+                                                           for(auto p : mp)
+                                                                   {
+                                                                               bucket[p.second].push_back(p.first);
+                                                                                       }
+
+                                                                                               vector<int> ans;
+
+                                                                                                       for(int i = nums.size(); i >= 1; i--)
+                                                                                                               {
+                                                                                                                           for(int x : bucket[i])
+                                                                                                                                       {
+                                                                                                                                                       ans.push_back(x);
+
+                                                                                                                                                                       if(ans.size() == k)
+                                                                                                                                                                                           return ans;
+                                                                                                                                                                                                       }
+                                                                                                                                                                                                               }
+
+                                                                                                                                                                                                                       return ans; 
+    }
+};
